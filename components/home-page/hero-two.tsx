3aef478afc7340d34d80febf26aafa4df21a805f
minh1971990 <DMinh197190@gmail.com>
2024-09-19 02:41:22 -0400

    Your commit message here

--- components/home-page/hero-two.tsx
+++ components/home-page/hero-two.tsx
@@ -4,8 +4,10 @@ import Image from "next/image";
 
 // Third-party library imports
 import Balancer from "react-wrap-balancer";
-import { Camera } from "lucide-react";
 import { BsDownload } from "react-icons/bs";
+import {Canvas} from '@react-three/fiber'
+import {OrbitControls} from '@react-three/drei'
+import {RoboModel} from './Scene'
 
 // Local component imports
 import { Section, Container } from "@/components/craft";
@@ -13,18 +15,20 @@ import { Button } from "@/components/ui/button";
 
 // Asset imports
 import Logo from "@/public/logo.svg";
+import { Suspense } from "react";
 
 const Hero = () => {
   return (
     <Section>
       <Container className="flex flex-col items-center text-center">
 
-        <h1 className="!mb-0">
+        <h1 className="!mb-15">
           <Balancer>
             Parallel
           </Balancer>
         </h1>
-        <h3 className="text-muted-foreground">
+        <RoboModel/>
+        <h3 className="text-muted-foreground !mt-0">
           <Balancer>
             A 2D Retro Top-Down thriller game project by <span className="text-red-600 font-bold text-10x2">Wabash</span> Computer Science seniors. 
           </Balancer>
